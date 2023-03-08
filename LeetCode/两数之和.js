f0433c5ca4d8b8593dea62fadf72de7f73c3ae7f
BreettySun <2058696174@qq.com>
2023-03-09 00:09:38 +0800

    23.03.09

---
+++ LeetCode/两数之和.js
@@ -0,0 +1,18 @@
+/**
+ * 两数之和
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let len = nums.length
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
---
+++ LeetCode/合并两个有序数组.js
@@ -0,0 +1,15 @@
+/**
+ * 合并两个有序数组
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  nums1.splice(m, nums1.length - m, ...nums2);
+  nums1.sort((a, b) => a - b);
+};
+
+let nums1 = [0], m = 0, nums2 = [2, 5, 6], n = 3
+merge(nums1, m, nums2, n)

1337755320b49d3603388b50515d0be9dfb8716d
BreettySun <2058696174@qq.com>
2023-03-11 00:16:02 +0800

    23.03.10

--- LeetCode/杨辉三角.js
+++ LeetCode/杨辉三角.js
@@ -28,4 +28,20 @@ var generate = function (numRows) {
   }
 };
 
+
+/**
+ * 官方解法
+ */
+var generate = function (numRows) {
+  const ret = [];
+  for (let i = 0; i < numRows; i++) {
+    const row = new Array(i + 1).fill(1);
+    for (let j = 1; j < row.length - 1; j++) {
+      row[j] = ret[i - 1][j - 1] + ret[i - 1][j];
+    }
+    ret.push(row);
+  }
+  return ret;
+};
+
 console.log(generate(4));

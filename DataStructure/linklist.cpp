fcec633db03cdc419e8b24b7ae214a2d23f11f1b
BreettySun <2058696174@qq.com>
2023-03-27 21:37:28 +0800

    23.03.27

--- DataStructure/linklist.cpp
+++ DataStructure/linklist.cpp
@@ -2,7 +2,7 @@
  * @Author: BreettySun
  * @Date: 2023-03-23 20:38:59
  * @LastEditors: BreettySun
- * @LastEditTime: 2023-03-23 21:28:46
+ * @LastEditTime: 2023-03-27 21:36:40
  * @FilePath: \Scream-Repo\DataStructure\linklist.cpp
  * @Description:
  *
@@ -10,10 +10,79 @@
  */
 
 #include <bits/stdc++.h>
+#define ElemType int
 using namespace std;
 
-int main()
-{
-  cout << "hello world" << endl;
-  return 0;
+typedef struct LNode {
+	ElemType data;
+	struct LNode *next;
+} LNode, *Linklist;
+
+/**
+ * @description:
+ * @param {Linklist} &L
+ * @return {bool}
+ */
+bool InitListNoHead(Linklist &L) {
+	L = nullptr;
+	return true;
+}
+
+/**
+ * @description:
+ * @param {Linklist} L
+ * @return {bool}
+ */
+bool EmptyNoHead(Linklist L) {
+	return L == nullptr;
+}
+
+/**
+ * @description:
+ * @param {Linklist} &L
+ * @return {bool}
+ */
+bool InitList(Linklist &L) {
+	L = (LNode *)malloc(sizeof(LNode));
+	if (L == nullptr) {
+		return false;
+	} else {
+		L->next = nullptr;
+		return true;
+	}
+}
+
+/**
+ * @description:
+ * @param {Linklist} L
+ * @return {bool}
+ */
+bool Empty(Linklist L) {
+	return L->next == nullptr;
+}
+
+/**
+ * @description:
+ * @param {Linklist} L
+ * @param {int} i
+ * @return {LNode}
+ */
+LNode *GetElem(Linklist L, int i) {
+	int j = 1;
+	LNode *p = L->next;
+	if (i == 0) {
+		return L;
+	} else if (i < 1) {
+		return nullptr;
+	}
+	while (p != nullptr && j < i) {
+		p = p->next;
+		j++;
+	}
+	return p;
+}
+
+int main() {
+	cout << "hello world" << endl;
+	return 0;
 }
